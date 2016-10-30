b6d7233741e737faf59373f611c43c7b12210502
Szilágyi Balázs <zyxneo@gmail.com>
2016-10-30 14:42:46 +0100

    route fix 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname + "/flexbox"),
+    path: './',
     publicPath: "/flexbox/",
   },
   module: {
