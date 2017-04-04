ab4da15277d3a74ee15fa687a21aec21dd2618b9
Balázs Szilágyi <bsz@seso.at>
2017-04-04 13:51:27 +0200

    tabBox example

--- src/pages/ExamplesPage.js
+++ src/pages/ExamplesPage.js
@@ -5,6 +5,18 @@ import examples from "../articles/examples.html";
 
 export default class ExamplesPage extends React.Component {
 
+  componentDidMount () {
+    var clickable = document.getElementsByClassName("tabBox__title");
+    for(var i = 0; i < clickable.length; i++) {
+      clickable[i].onclick = function() {
+        for(var i = 0; i < clickable.length; i++) {
+          clickable[i].classList.remove("is-active");
+        }
+        this.classList.add("is-active");
+      }
+    }
+  }
+
   render() {
 
       return (
